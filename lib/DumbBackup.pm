1c3d2d0baba6d4a8640788f84d86ab3b7705ce95
Philippe Bruhat (BooK) <book@cpan.org>
2022-01-22 16:52:20 +0100

    DumbBackup: the front end to all subcommands
    Run it with `unpacked-dumbbackup`, which we will later
    fatpack into `dumbbackup`, so it's still a single
    executable to download and install.

---
+++ lib/DumbBackup.pm
@@ -0,0 +1,23 @@
+package DumbBackup;
+use 5.024;
+use warnings;
+
+use Module::Runtime qw( require_module );
+
+use feature 'signatures';
+no warnings 'experimental::signatures';
+
+my %subcommand = (
+);
+
+sub run ( $self, @args ) {
+    my $cmd = shift @args || '';
+    die "Unknown subcommand $cmd\n"
+      if !exists $subcommand{$cmd};
+
+    my $class = "DumbBackup::$subcommand{$cmd}";
+    require_module($class);
+    $class->new( arguments => \@args )->call;
+}
+
+1;
